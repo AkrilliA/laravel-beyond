dbe08ffd1b3b211e33a27a94ce584e04c16ace2c
Alexander Gaal <alexander.gaal@live.at>
2022-02-15 14:51:43 +0100

    pr improvements

--- src/Commands/SetupCommand.php
+++ src/Commands/SetupCommand.php
@@ -4,10 +4,9 @@ namespace Regnerisch\LaravelBeyond\Commands;
 
 use Illuminate\Console\Command;
 use Regnerisch\LaravelBeyond\Actions\ChangeComposerAutoloaderAction;
-use Regnerisch\LaravelBeyond\Actions\DeleteFolderAction;
+use Regnerisch\LaravelBeyond\Actions\DeleteAction;
 use Regnerisch\LaravelBeyond\Actions\MoveAndRefactorFileAction;
 use Regnerisch\LaravelBeyond\Actions\RefactorFileAction;
-use Regnerisch\LaravelBeyond\Actions\ReplaceFileContentAction;
 
 class SetupCommand extends Command
 {
@@ -19,8 +18,7 @@ class SetupCommand extends Command
         protected MoveAndRefactorFileAction $moveAndRefactorFileAction,
         protected RefactorFileAction $refactorFileAction,
         protected ChangeComposerAutoloaderAction $changeComposerAutoloaderAction,
-        protected ReplaceFileContentAction $replaceFileContentAction,
-        protected DeleteFolderAction $deleteFolderAction,
+        protected DeleteAction $deleteAction,
     ) {
         parent::__construct();
     }
@@ -76,7 +74,7 @@ class SetupCommand extends Command
         $this->prepareBootstrap();
 
         // Rewrite configs
-        $this->replaceFileContentAction->execute(
+        $this->refactorFileAction->execute(
             base_path() . '/config/auth.php',
             [
                 'App\Models\User::class' => 'Domain\Users\Models\User::class'
@@ -87,7 +85,7 @@ class SetupCommand extends Command
         $this->changeComposerAutoloaderAction->execute();
 
         // Delete app folder
-        $this->deleteFolderAction->execute(base_path() . '/app');
+        $this->deleteAction->execute(base_path() . '/app');
     }
 
     protected function moveMiddlewares(): void
