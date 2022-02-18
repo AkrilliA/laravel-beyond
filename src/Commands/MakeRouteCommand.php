b2ed11cb07867a3b6882d5c86bca324f2eedb413
Alexander Gaal <alexander.gaal@live.at>
2022-02-18 20:57:33 +0100

    some improvements and add information to console to add route to RSP

--- src/Commands/MakeRouteCommand.php
+++ src/Commands/MakeRouteCommand.php
@@ -13,13 +13,24 @@ class MakeRouteCommand extends Command
     public function handle()
     {
         $application = $this->argument('application');
+        $applicationNameLowerCase = strtolower($application);
 
         beyond_copy_stub(
             'routes.stub',
-            app_path() . "/../src/App/{$application}/routes/web.php",
+            app_path() . "/../src/App/{$application}/routes/{$applicationNameLowerCase}.php",
             [
-                '{{ application }}' => $application,
+                '{{ application }}' => $applicationNameLowerCase,
             ]
+        );
+
+
+        $this->info(
+            "Please add following route entry to your RouteServiceProvider" . PHP_EOL . PHP_EOL .
+
+                "\tRoute::prefix('api')" . PHP_EOL .
+                    "\t\t->middleware('api')" . PHP_EOL .
+                    "\t\t->namespace($this->namespace)" . PHP_EOL .
+                    "\t\t->group(base_path('routes/api.php'));"
         );
     }
 }
