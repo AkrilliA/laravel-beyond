533a6d744558774ab2d43b9eaee1c46be75d6933
Alexander Gaal <alexander.gaal@live.at>
2022-02-18 23:23:09 +0100

    add plain policy

--- src/Commands/MakePolicyCommand.php
+++ src/Commands/MakePolicyCommand.php
@@ -7,7 +7,7 @@ use Regnerisch\LaravelBeyond\Resolvers\DomainNameSchemaResolver;
 
 class MakePolicyCommand extends Command
 {
-    protected $signature = 'beyond:make:policy {name}';
+    protected $signature = 'beyond:make:policy {name} {--model=}';
 
     protected $description = 'Make a new policy';
 
@@ -15,16 +15,30 @@ class MakePolicyCommand extends Command
     {
         try {
             $name = $this->argument('name');
+            $model = $this->option('model');
 
             $schema = new DomainNameSchemaResolver($name);
 
-            beyond_copy_stub(
-                'policy.stub',
-                base_path() . '/src/Domain/' . $schema->getPath('Policies') . '.php',
-                [
+            if ($model) {
+                $stub = 'policy.stub';
+                $replacements = [
                     '{{ domain }}' => $schema->getDomainName(),
                     '{{ className }}' => $schema->getClassName(),
-                ]
+                    '{{ modelName }}' => $model,
+                    '{{ modelVariable }}' => mb_strtolower($model),
+                ];
+            } else {
+                $stub = 'policy.plain.stub';
+                $replacements = [
+                    '{{ domain }}' => $schema->getDomainName(),
+                    '{{ className }}' => $schema->getClassName(),
+                ];
+            }
+
+            beyond_copy_stub(
+                $stub,
+                base_path() . '/src/Domain/' . $schema->getPath('Policies') . '.php',
+                $replacements
             );
         } catch (\Exception $exception) {
             $this->error($exception->getMessage());
