49157b635ac557d67a5f8c4e70609bed32d081f2
Jonas Regner <jonas.regner@espresso-tutorials.com>
2022-05-09 14:25:32 +0200

    fix(MakeCommandCommand): fix wrong argument access

--- src/Commands/MakeCommandCommand.php
+++ src/Commands/MakeCommandCommand.php
@@ -6,7 +6,7 @@ use Illuminate\Console\Command;
 
 class MakeCommandCommand extends Command
 {
-    protected $signature = 'beyond:make:command {name} {--command=command:name}';
+    protected $signature = 'beyond:make:command {className} {--command=command:name}';
 
     protected $description = 'Make a new command';
 
