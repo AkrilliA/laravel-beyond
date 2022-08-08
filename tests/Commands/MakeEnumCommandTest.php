75047da0c804a2e59fba9b071fa92a418960f194
Jonas Regner <jonas.regner@espresso-tutorials.com>
2022-08-08 13:47:08 +0200

    test: optimize test

--- tests/Commands/MakeEnumCommandTest.php
+++ tests/Commands/MakeEnumCommandTest.php
@@ -6,7 +6,7 @@ test('cannot make enum on php 8.0', function () {
     $this->artisan('beyond:make:enum')
         ->expectsOutput('Your version 8.0 does not match the required version 8.1 of this command.')
         ->assertExitCode(1);
-})->skip(version_compare(PHP_VERSION, '8.1', '>='));
+})->skip(PHP_VERSION_ID >= 80100);
 
 test('can make enum', function () {
     $this->artisan('beyond:make:enum User/UserStatusEnum');
@@ -15,4 +15,4 @@ test('can make enum', function () {
         ->toBeFile()
         ->toMatchNamespaceAndClassName()
         ->toPlaceholdersBeReplaced();
-})->skip(version_compare(PHP_VERSION, '8.1', '<'));
+})->skip(PHP_VERSION_ID < 80100);
