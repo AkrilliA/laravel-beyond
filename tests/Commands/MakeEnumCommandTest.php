ac6581a44e7de14a1f8c02de2abcb8cc2ca9ff76
Alexander Gaal <alexander.gaal@live.at>
2022-08-08 13:42:26 +0200

    add test for php 8 and php 8.1 for enums

--- tests/Commands/MakeEnumCommandTest.php
+++ tests/Commands/MakeEnumCommandTest.php
@@ -2,6 +2,12 @@
 
 namespace Tests\Commands;
 
+test('cannot make enum on php 8.0', function () {
+    $this->artisan('beyond:make:enum')
+        ->expectsOutput('Your version 8.0 does not match the required version 8.1 of this command.')
+        ->assertExitCode(1);
+})->skip(version_compare(PHP_VERSION, '8.1', '>='));
+
 test('can make enum', function () {
     $this->artisan('beyond:make:enum User/UserStatusEnum');
 
@@ -9,4 +15,4 @@ test('can make enum', function () {
         ->toBeFile()
         ->toMatchNamespaceAndClassName()
         ->toPlaceholdersBeReplaced();
-});
+})->skip(version_compare(PHP_VERSION, '8.1', '<'));
