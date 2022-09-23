63b6dbc27addb5c00eabf2356b6d7fab44f7eef4
Krishna Bhandari <krishna_bhandari2014@outlook.com>
2022-09-23 21:17:19 +0545

    Update MakeModelCommandTest.php

--- tests/Commands/MakeModelCommandTest.php
+++ tests/Commands/MakeModelCommandTest.php
@@ -31,3 +31,23 @@ test('can make model, factory and migration are created', function () {
         ->toBeFile()
         ->toPlaceholdersBeReplaced();
 });
+
+
+test('can make model, factory and migration are created using shortcuts', function () {
+    $this->artisan('beyond:make:model User/User -mf --force');
+
+    expect(base_path() . '/src/Domain/User/Models/User.php')
+        ->toBeFile()
+        ->toMatchNamespaceAndClassName()
+        ->toPlaceholdersBeReplaced();
+
+    $date = Carbon::parse()->format('Y_m_d_his');
+
+    expect(base_path() . "/database/migrations/{$date}_create_users_table.php")
+        ->toBeFile()
+        ->toPlaceholdersBeReplaced();
+
+    expect(base_path() . '/database/factories/UserFactory.php')
+        ->toBeFile()
+        ->toPlaceholdersBeReplaced();
+});
