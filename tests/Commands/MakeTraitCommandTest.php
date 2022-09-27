e31300b36341460f336a694a2d022cec3d4a65c4
Dimitri Bocquet <dimitri@manajet.com>
2022-09-27 14:35:37 +0200

    better unit tests

--- tests/Commands/MakeTraitCommandTest.php
+++ tests/Commands/MakeTraitCommandTest.php
@@ -7,45 +7,17 @@ test('can make trait', function () {
 
     expect(base_path() . '/src/Domain/Users/Traits/HasActivationCodeTrait.php')
         ->toBeFile()
-        ->toPlaceholdersBeReplaced();
+        ->toMatchNamespaceAndClassName()
+        ->toPlaceholdersBeReplaced()
+        ->toFileContains('trait HasActivationCodeTrait');
 });
 
 test('can make support trait', function () {
     $this->artisan('beyond:make:trait HasActivationCodeTrait --support');
 
-    expect(base_path() . '/src/Support/Packages/Laravel/Traits/HasActivationCodeTrait.php')
+    expect(base_path() . '/src/Support/Traits/HasActivationCodeTrait.php')
         ->toBeFile()
-        ->toPlaceholdersBeReplaced();
-});
-
-test('namespace is correct', function () {
-    $this->artisan('beyond:make:trait Users/HasActivationCodeTrait');
-
-    $file = base_path() . '/src/Domain/Users/Traits/HasActivationCodeTrait.php';
-    $content = file_get_contents($file);
-    expect($content)->toContain('namespace Domain\Users\Traits;');
-});
-
-test('support namespace is correct', function () {
-    $this->artisan('beyond:make:trait HasActivationCodeTrait --support');
-
-    $file = base_path() . '/src/Support/Packages/Laravel/Traits/HasActivationCodeTrait.php';
-    $content = file_get_contents($file);
-    expect($content)->toContain('namespace Support\Packages\Laravel\Traits;');
-});
-
-test('starts with trait', function () {
-    $this->artisan('beyond:make:trait Users/HasActivationCodeTrait');
-
-    $file = base_path() . '/src/Domain/Users/Traits/HasActivationCodeTrait.php';
-    $content = file_get_contents($file);
-    expect($content)->toContain('trait HasActivationCodeTrait');
-});
-
-test('support starts with trait', function () {
-    $this->artisan('beyond:make:trait HasActivationCodeTrait --support');
-
-    $file = base_path() . '/src/Support/Packages/Laravel/Traits/HasActivationCodeTrait.php';
-    $content = file_get_contents($file);
-    expect($content)->toContain('trait HasActivationCodeTrait');
+        ->toPlaceholdersBeReplaced()
+        ->toMatchNamespaceAndClassName()
+        ->toFileContains('trait HasActivationCodeTrait');
 });
