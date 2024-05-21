82f9bfdef273942d5ae83b2c970d8ac745a1728b
Jonas Regner <regnerjonas@protonmail.com>
2024-05-21 19:49:07 +0200

    test: remove old tests

--- src/Commands/MakeModelCommand.php
+++ src/Commands/MakeModelCommand.php
@@ -9,7 +9,7 @@ use Illuminate\Support\Str;
 
 final class MakeModelCommand extends DomainCommand
 {
-    protected $signature = 'beyond:make:model {name} {--f|factory} {--m|migration} {--force}';
+    protected $signature = 'beyond:make:model {name} {--force}';
 
     protected $description = 'Make a new model';
 
@@ -21,34 +21,5 @@ final class MakeModelCommand extends DomainCommand
     public function getType(): Type
     {
         return new Type('Model');
-    }
-
-    public function setup(NameResolver $nameResolver): void
-    {
-        if ($this->option('migration')) {
-            $command = new MakeMigrationCommand();
-            $fqn = $command->getNameResolver($nameResolver->getAppOrDomain().'.create_fake_table');
-
-            $this->addOnSuccess(function (string $namespace, string $className) use ($fqn) {
-                $this->call(MakeMigrationCommand::class, [
-                    'name' => $fqn->getAppOrDomain().'.create_'.Str::of($className)->pluralStudly()->lower().'_table',
-                ]);
-            });
-
-            // Add MakeFactoryCommand!
-            //            if ($this->option('factory')) {
-            //                $fileName = $className.'Factory';
-            //
-            //                beyond_copy_stub(
-            //                    'factory.stub',
-            //                    base_path()."/modules/$module/Infrastructure/factories/$fileName.php",
-            //                    [
-            //                        '{{ namespace }}' => $namespace,
-            //                        '{{ model }}'     => $fileName,
-            //                    ],
-            //                    $force
-            //                );
-            //            }
-        }
     }
 }
